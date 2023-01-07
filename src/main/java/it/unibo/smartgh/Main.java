2ba0eec062e7b8f789efb38e9c3f467a3e355e14
Maria <mengozzi.maria@gmail.com>
2023-01-07 17:12:19 +0100

    chore: add javafx dependencies

--- src/main/java/it/unibo/smartgh/Main.java
+++ src/main/java/it/unibo/smartgh/Main.java
@@ -1,7 +1,23 @@
 package it.unibo.smartgh;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("entry point");
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 300);
+
+        stage.setTitle("JavaFX Demo");
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
