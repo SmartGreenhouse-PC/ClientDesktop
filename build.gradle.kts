9376d42f58a077dd10e69104e750c428e657280e
Maria <mengozzi.maria@gmail.com>
2023-01-07 17:12:19 +0100

    chore: add javafx dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,28 @@ repositories {
     mavenCentral()
 }
 
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+// All required for OOP
+val supportedPlatforms = listOf("linux", "mac", "win")
+
 dependencies {
     implementation(libs.bundles.vertx.dependencies)
     implementation(libs.gson)
+    // JavaFX: comment out if you do not need them
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:13:$platform")
+        }
+    }
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
 }
-
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
