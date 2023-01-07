92928b34c6dde226f5bb60cca1b889024979ae9f
Maria <mengozzi.maria@gmail.com>
2023-01-07 17:40:09 +0100

    chore: add javafx plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.javafx)
 }
 
 group = "it.unibo.smartgh"
@@ -10,25 +11,14 @@ repositories {
     mavenCentral()
 }
 
-val javaFXModules = listOf(
-    "base",
-    "controls",
-    "fxml",
-    "swing",
-    "graphics"
-)
-// All required for OOP
-val supportedPlatforms = listOf("linux", "mac", "win")
+javafx {
+    version = "16"
+    modules = listOf("javafx.controls")
+}
 
 dependencies {
     implementation(libs.bundles.vertx.dependencies)
     implementation(libs.gson)
-    // JavaFX: comment out if you do not need them
-    for (platform in supportedPlatforms) {
-        for (module in javaFXModules) {
-            implementation("org.openjfx:javafx-$module:13:$platform")
-        }
-    }
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
 }
