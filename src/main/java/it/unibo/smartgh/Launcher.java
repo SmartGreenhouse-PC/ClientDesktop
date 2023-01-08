bf16b986e7db3a592ee9e739a1c6a3f040d29ed7
Elena Yan <elena.yan@studio.unibo.it>
2023-01-08 18:50:51 +0100

    feat: setup base application

--- src/main/java/it/unibo/smartgh/Launcher.java
+++ src/main/java/it/unibo/smartgh/Launcher.java
@@ -1,11 +1,16 @@
 package it.unibo.smartgh;
 
+import it.unibo.smartgh.controller.ApplicationController;
+import it.unibo.smartgh.controller.ApplicationControllerImpl;
+import it.unibo.smartgh.view.ApplicationView;
+import it.unibo.smartgh.view.ApplicationViewImpl;
+
 public final class Launcher {
 
-    private Launcher() {}
-
     public static void main(final String[] args) {
-        Main.main(args);
+        ApplicationView view = new ApplicationViewImpl();
+        ApplicationController controller = new ApplicationControllerImpl(view);
+        controller.start();
     }
 
 }
