673b349be4a3d22eb5bd5a919d839db5ad195510
Elena Yan <elena.yan@studio.unibo.it>
2023-01-09 11:38:52 +0100

    feat(homepage): add homepage view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module it.unibo.smartgh.view {
 
     opens it.unibo.smartgh.view to javafx.fxml;
     exports it.unibo.smartgh.view;
+    exports it.unibo.smartgh.view.homepage;
+    opens it.unibo.smartgh.view.homepage to javafx.fxml;
 }
