2854a8f1809966bcd7d25ca10095fe0f7212078b
Elena Yan <elena.yan@studio.unibo.it>
2023-01-10 15:46:18 +0100

    feat(homepage): start implement homepage controller

--- src/main/java/it/unibo/smartgh/controller/homepage/HomepageControllerImpl.java
+++ src/main/java/it/unibo/smartgh/controller/homepage/HomepageControllerImpl.java
@@ -1,13 +1,34 @@
 package it.unibo.smartgh.controller.homepage;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 import it.unibo.smartgh.view.homepage.HomepageView;
 import it.unibo.smartgh.view.homepage.HomepageViewImpl;
 
 public class HomepageControllerImpl implements HomepageController {
 
+    private static final int PORT = 8888;
+    private static final String HOST = "localhost";
     private final HomepageView view;
+    private final Vertx vertx;
+    private final String id;
 
-    public HomepageControllerImpl(HomepageViewImpl homepageView) {
+    public HomepageControllerImpl(HomepageViewImpl homepageView, String id) {
         this.view = homepageView;
+        this.id = id;
+        this.vertx = Vertx.vertx();
+    }
+
+    private void updateView() {
+        WebClient client = WebClient.create(vertx);
+        client.get(PORT, HOST, "clientCommunication/greenhouse")
+                .addQueryParam("id", id)
+                .as(BodyCodec.string())
+                .send()
+                .onSuccess(res -> {
+                    System.out.println(res.body()); //todo
+                })
+                .onFailure(Throwable::printStackTrace);
     }
 }
