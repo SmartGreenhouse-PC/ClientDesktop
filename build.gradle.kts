b9947d4094bc93afea583a23095d0e5dff0483ba
Maria <mengozzi.maria@gmail.com>
2023-01-18 17:26:58 +0100

    chore: add jar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,38 @@
 plugins {
+    java
     application
+    id("com.github.johnrengelman.shadow") version "5.2.0"
     alias(libs.plugins.gitSemVer)
-    alias(libs.plugins.javafx)
 }
 
 group = "it.unibo.smartgh"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
+project.setProperty("mainClassName", "it.unibo.smartgh.Launcher")
 
 repositories {
     mavenCentral()
 }
 
-javafx {
-    version = "16"
-    modules = listOf("javafx.controls", "javafx.fxml")
-}
+// List of JavaFX modules you need. Comment out things you are not using.
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+// All required for OOP
+val supportedPlatforms = listOf("linux", "mac", "win")
 
 dependencies {
     implementation(libs.bundles.vertx.dependencies)
     implementation(libs.gson)
+    // JavaFX: comment out if you do not need them
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:13:$platform")
+        }
+    }
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
 }
