9d583cba0dd3204b4789087d33e6198fd54182cf
Elena Yan <elena.yan@studio.unibo.it>
2023-01-31 00:24:38 +0100

    refactor: refactor code

--- src/main/java/it/unibo/smartgh/view/homepage/HomepageViewImpl.java
+++ src/main/java/it/unibo/smartgh/view/homepage/HomepageViewImpl.java
@@ -8,7 +8,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
