d399b704f81b93d0eab5374871553b26edcca1ba
Elena Yan <elena.yan@studio.unibo.it>
2023-02-01 11:48:32 +0100

    test(view): test parameter history

--- src/test/java/it/unibo/smartgh/view/parameter/ParameterPageViewImplTest.java
+++ src/test/java/it/unibo/smartgh/view/parameter/ParameterPageViewImplTest.java
@@ -5,6 +5,8 @@ import it.unibo.smartgh.model.plant.PlantParameter;
 import it.unibo.smartgh.model.plant.PlantParameterBuilder;
 import it.unibo.smartgh.view.AbstractViewTest;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.awaitility.Duration;
@@ -15,6 +17,9 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static org.awaitility.Awaitility.await;
@@ -46,20 +51,15 @@ class ParameterPageViewImplTest extends AbstractViewTest {
 
     @Test
     public void testParameterValue(FxRobot robot) {
-        final Label parameterName = robot.lookup("#parameterName").queryAs(Label.class);
-        final Label minValue = robot.lookup("#minValue").queryAs(Label.class);
-        final Label maxValue = robot.lookup("#maxValue").queryAs(Label.class);
+        final Labeled parameterName = robot.lookup("#parameterName").queryLabeled();
+        final Labeled minValue = robot.lookup("#minValue").queryLabeled();
+        final Labeled maxValue = robot.lookup("#maxValue").queryLabeled();
         final ImageView parameterImg = robot.lookup("#img").queryAs(ImageView.class);
         final Label currentValueLabel = robot.lookup("#currentValue").queryAs(Label.class);
         this.view.initializePage(type, plantParameter.getMin().toString(), plantParameter.getMax().toString(),
                 this.parameter.getCurrentValue().getValue().toString() +  plantParameter.getUnit(),
                 this.parameter.getHistoryAsMap(), "alarm");
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         await().pollInterval(Duration.TWO_HUNDRED_MILLISECONDS).atMost(Duration.FIVE_SECONDS).untilAsserted(() -> {
             assertThat(parameterName).hasText(type.getTitle());
             assertThat(minValue).hasText(plantParameter.getMin().toString());
@@ -67,6 +67,26 @@ class ParameterPageViewImplTest extends AbstractViewTest {
             assertNotNull(parameterImg.getImage());
             assertFalse(parameterImg.getImage().isError());
             assertThat(currentValueLabel).hasText(currentValue.getValue() + plantParameter.getUnit());
+        });
+    }
+
+    @Test
+    public void testParameterHistory(FxRobot robot) {
+        final TableView<ParameterPageViewImpl.Row> table = robot.lookup("#table").queryTableView();
+        final DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+
+        this.view.initializePage(type, plantParameter.getMin().toString(), plantParameter.getMax().toString(),
+                this.parameter.getCurrentValue().getValue().toString() +  plantParameter.getUnit(),
+                this.parameter.getHistoryAsMap(), "alarm");
+
+        await().pollInterval(Duration.TWO_HUNDRED_MILLISECONDS).atMost(Duration.FIVE_SECONDS).untilAsserted(() -> {
+            table.getItems().forEach(row -> {
+                try {
+                    assertTrue(this.history.contains(new ParameterValueImpl(ID, formatter.parse(row.getDate()), row.getValue())));
+                } catch (ParseException e) {
+                    fail();
+                }
+            });
         });
     }
 }
